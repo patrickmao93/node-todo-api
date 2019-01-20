4a8bde4372c4dbf192bc802b7e5b85c0d37af992
Patrick <patrickmao93@gmail.com>
2019-01-20 11:27:22 +0800

    added get todos route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectId } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -30,6 +31,23 @@ app.get("/todos", (req, res) => {
       res.status(400).send(err);
     }
   );
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = req.params.id;
+  if (!ObjectId.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.status(200).send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(8000, () => {
